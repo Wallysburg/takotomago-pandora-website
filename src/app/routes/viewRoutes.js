8a8f4d6ebb78167dccb289ab5f348d4a4abcedd5
Charlie Davis <cdavis@accusoft.com>
2017-03-01 22:14:36 -0500

    fixed deploy script

--- src/app/routes/viewRoutes.js
+++ src/app/routes/viewRoutes.js
@@ -3,7 +3,6 @@ import { Route, IndexRoute } from 'react-router';
 import Layout from './../views/Layout';
 import NotFoundPage from './../views/NotFound';
 import IndexPage from './../views/Index';
-// import Pandora from './..views/Pandora';
 
 const componentRoutes = (
   <Route path="/" component={Layout}>
