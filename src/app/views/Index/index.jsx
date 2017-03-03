06e2a8535ae0efadb1ce11ab40968f034068e8d8
Charlie Davis <cdavis@accusoft.com>
2017-03-02 23:05:55 -0500

    added links and stuff

--- src/app/views/Index/index.jsx
+++ src/app/views/Index/index.jsx
@@ -26,13 +26,15 @@ const IndexPage = () => (
       <Pandora/>
       <div className={styles.pageContainer2}>
         <div className={styles.textContainer} >
-         <p>Tako Tomago is an artist based in Los Angeles, California.</p>
+         <p>Tako Tomago is an artist based in <a href="https://www.google.com/maps/place/Los+Angeles,+CA/@34.0201812,-118.6919205,10z/data=!3m1!4b1!4m5!3m4!1s0x80c2c75ddc27da13:0xe22fdf6f254608f4!8m2!3d34.0522342!4d-118.2436849">LA</a>.</p>
          <br/>
-         <p>You can listen to his music here.</p>
+         <p>You can listen to his music <a href="http://soundcloud.com/takotomago">Here</a>.</p>
           <br/>
-         <p>You can read about upcoming releases here.</p>
+         <p>You can read about upcoming releases <a href="http://soundcloud.com/takotomago">Here</a>.</p>
          <br/>
-         <p>You can talk to him here.</p>
+         <p>Upcoming <a href="http://twitter.com/takotomago">Releases</a>.</p>
+         <br/>
+         <p>You can talk to him <a href="mailto:takotomago@gmail.com?Subject=Hello">Here</a>.</p>
          <br/>
         </div>
       </div>
